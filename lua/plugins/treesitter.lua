88d4dd831cedeb45414f1cf350f879937631ad83
Marco Antônio <ma.souza.junior@gmail.com>
2024-03-21 15:55:55 -0300

    fea: setup treesitter

---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,78 @@
+local ensure_installed = {
+  -- recommended
+  "vim",
+  "vimdoc",
+  "query",
+  "markdown",
+  "markdown_inline",
+
+  -- defaults
+  "vim",
+  "lua",
+
+  -- web dev
+  "html",
+  "css",
+  "javascript",
+  "typescript",
+  "tsx",
+  "json",
+
+  -- low level
+  "c",
+  "zig",
+  "rust",
+}
+
+local textobjects = {
+  select = {
+    enable = true,
+
+    lookahead = true,
+
+    keymaps = {
+      ["af"] = "@function.outer",
+      ["if"] = "@function.inner",
+      ["ac"] = "@class.outer",
+      ["ic"] = {
+        query = "@class.inner",
+        desc = "Select inner part of a class region",
+      },
+      ["as"] = {
+        query = "@scope",
+        query_group = "locals",
+        desc = "Select language scope",
+      },
+    },
+    selection_modes = {
+      ["@parameter.outer"] = "v", -- charwise
+      ["@function.outer"] = "V", -- linewise
+      ["@class.outer"] = "<c-v>", -- blockwise
+    },
+    include_surrounding_whitespace = true,
+  },
+}
+
+return {
+  "nvim-treesitter/nvim-treesitter",
+  dependencies = { "nvim-treesitter/nvim-treesitter-textobjects" },
+  init = function()
+    require("nvim-treesitter.configs").setup {
+      textobjects = textobjects,
+      ensure_installed = ensure_installed,
+      auto_install = true,
+      indent = { enable = true },
+      highlight = { enable = true },
+      incremental_selection = {
+        enable = true,
+        keymaps = {
+          init_selection = "<leader>ss",
+          node_incremental = "<leader>si",
+          scope_incremental = "<leader>sc",
+          node_decremental = "<leader>sd",
+        },
+      },
+    }
+  end,
+}
+
