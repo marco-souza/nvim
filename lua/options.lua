edb305a35ec3ca33e5d38db54605bccca1f795ab
Marco Antônio <ma.souza.junior@gmail.com>
2024-03-21 15:04:12 -0300

    enh: move my options

--- lua/options.lua
+++ lua/options.lua
@@ -1,6 +1,48 @@
 require "nvchad.options"
 
--- add yours here!
+--- Editor settings module
 
--- local o = vim.o
--- o.cursorlineopt ='both' -- to enable cursorline!
+-- line numbers
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+vim.opt.splitbelow = true
+vim.opt.splitright = true
+
+-- text wrap
+vim.opt.wrap = false
+
+-- tabs
+vim.opt.expandtab = true
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 0 -- fallback to tabstop
+
+-- clipboard
+vim.opt.clipboard = "unnamedplus"
+
+-- padding
+vim.opt.scrolloff = 999
+
+--- visual editor
+vim.opt.virtualedit = "block"
+
+--- inccommand
+vim.opt.inccommand = "split"
+
+--- ignorecase for searching commands
+vim.opt.ignorecase = true
+
+--- terminal colors
+vim.opt.termguicolors = true
+
+-- fold
+vim.opt.foldmethod = "expr"
+vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
+vim.opt.foldlevelstart = 1
+
+-- persistent undo
+vim.opt.undodir = vim.fn.expand("~/.config/nvim.undo")
+vim.opt.undofile = true
+
+-- leader
+vim.g.mapleader = " "
