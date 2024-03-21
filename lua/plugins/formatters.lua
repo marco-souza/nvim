d05df521ef9d1d90802420f4f4bff09b7028adb7
Marco Antônio <ma.souza.junior@gmail.com>
2024-03-21 16:15:16 -0300

    fea: setup formatters

---
+++ lua/plugins/formatters.lua
@@ -0,0 +1,31 @@
+local options = {
+  formatters_by_ft = {
+    lua = { "stylua" },
+    css = { "prettier" },
+    html = { "prettier" },
+    markdown = { "prettier" },
+    -- Conform will run multiple formatters sequentially
+    python = { "isort", "black" },
+    -- Use a sub-list to run only the first available formatter
+    javascript = { "eslint", { "prettierd", "prettier" } },
+    -- Conform will run multiple formatters sequentially
+    go = { "goimports", "gofmt" },
+    -- Use the "*" filetype to run formatters on all filetypes.
+    ["*"] = { "codespell" },
+    -- Use the "_" filetype to run formatters on filetypes that don't
+    -- have other formatters configured.
+    ["_"] = { "trim_whitespace" },
+  },
+
+  format_on_save = {
+    -- These options will be passed to conform.format()
+    timeout_ms = 500,
+    lsp_fallback = true,
+  },
+}
+
+return {
+  "stevearc/conform.nvim",
+  event = "LspAttach",
+  opts = options,
+}
