a394b3f44e5eee74f402fa7cef579f7668eb74dd
Marco Antônio <ma.souza.junior@gmail.com>
2024-03-21 15:14:32 -0300

    fea: add notes stuff

---
+++ lua/plugins/notes.lua
@@ -0,0 +1,38 @@
+return {
+  {
+    "epwalsh/obsidian.nvim",
+    version = "*", -- recommended, use latest release instead of latest commit
+    lazy = true,
+    ft = "markdown",
+    dependencies = {
+      -- Required.
+      "nvim-lua/plenary.nvim",
+    },
+    opts = {
+      workspaces = {
+        { name = "notes", path = "~/w/marco-souza/notes" },
+      },
+    },
+  },
+
+  {
+    "nvim-neorg/neorg",
+    ft = "norg",
+    build = ":Neorg sync-parsers",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    opts = {
+      load = {
+        ["core.defaults"] = {},  -- Loads default behaviour
+        ["core.concealer"] = {}, -- Adds pretty icons to your documents
+        ["core.dirman"] = {      -- Manages Neorg workspaces
+          config = {
+            workspaces = {
+              notes = "~/workspace/marco-souza/notes",
+            },
+          },
+        },
+      },
+    },
+  },
+}
+
