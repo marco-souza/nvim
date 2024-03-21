1a52922e96ef3c1b8ba62693e54d025b91104852
Marco Antônio <ma.souza.junior@gmail.com>
2024-03-21 15:59:40 -0300

    fea: add miscellaneous

---
+++ lua/plugins/misc.lua
@@ -0,0 +1,66 @@
+return {
+  { "wakatime/vim-wakatime", event = "VeryLazy" },
+  { "nvim-ts-autotag", opts = {} },
+  {
+    "max397574/better-escape.nvim",
+    event = "InsertEnter",
+    config = true,
+  },
+  {
+    "windwp/nvim-ts-autotag",
+    event = "InsertEnter",
+    config = true,
+  },
+  {
+    "kylechui/nvim-surround",
+    event = "InsertEnter",
+    version = "*", -- Use for stability; omit to use `main` branch for the latest features
+    config = true,
+  },
+  {
+    "dkarter/bullets.vim",
+    event = "InsertEnter",
+    config = function()
+      vim.g.bullets_enabled_file_types = {
+        "markdown",
+        "text",
+        "gitcommit",
+        "scratch",
+      }
+    end,
+  },
+  {
+    "nvim-tree/nvim-tree.lua",
+    opts = {
+      git = { enable = true },
+      renderer = {
+        highlight_git = true,
+        icons = {
+          show = { git = true },
+        },
+      },
+    },
+  },
+  {
+    "aca/emmet-ls",
+    event = "InsertEnter",
+    config = function()
+      local lspconfig = require "lspconfig"
+      local capabilities = vim.lsp.protocol.make_client_capabilities()
+      capabilities.textDocument.completion.completionItem.snippetSupport = true
+
+      lspconfig.emmet_ls.setup {
+        capabilities = capabilities,
+        init_options = {
+          html = {
+            options = {
+              -- For possible options, see: https://github.com/emmetio/emmet/blob/master/src/config.ts#L79-L267
+              ["bem.enabled"] = true,
+            },
+          },
+        },
+      }
+    end,
+ },
+}
+
