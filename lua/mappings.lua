88985e610ba1a0dca0053ffe13a7270bed866866
Marco Antônio <ma.souza.junior@gmail.com>
2024-03-21 15:07:37 -0300

    enh: move my keymaps

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -1,10 +1,26 @@
 require "nvchad.mappings"
 
--- add yours here
-
 local map = vim.keymap.set
 
+-- nvchad
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
--- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
+local opts = { noremap = true, silent = true }
+
+-- Normal --
+-- Better window navigation
+map("n", "<C-h>", "<C-w>h", opts)
+map("n", "<C-j>", "<C-w>j", opts)
+map("n", "<C-k>", "<C-w>k", opts)
+map("n", "<C-l>", "<C-w>l", opts)
+-- Close window
+map("n", "<leader>x", ":x<CR>", opts)
+
+-- Naviagate buffers
+map("n", "<S-l>", ":bnext<CR>", opts)
+map("n", "<S-h>", ":bprevious<CR>", opts)
+
+-- Move text up and down
+map("n", "<A-j>", "<Esc>:m .+1<CR>==g", opts)
+map("n", "<A-k>", "<Esc>:m .-2<CR>==g", opts)
