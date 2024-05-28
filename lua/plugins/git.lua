c96e0f0a437d343588354d2321f3582e3181be49
Marco Antônio <ma.souza.junior@gmail.com>
2024-05-28 16:15:34 -0300

    fea: use neogit for neovim 0.10

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -1,18 +1,16 @@
 return {
-  {
-    "NeogitOrg/neogit",
-    config = true,
-    tag = "v0.0.1",
-    dependencies = {
-      "nvim-lua/plenary.nvim", -- required
-      "sindrets/diffview.nvim", -- optional - Diff integration
+  "NeogitOrg/neogit",
+  config = true,
+  tag = "v1.0.0",
+  dependencies = {
+    "nvim-lua/plenary.nvim",    -- required
+    "sindrets/diffview.nvim",   -- optional - Diff integration
 
-      -- Only one of these is needed, not both.
-      "nvim-telescope/telescope.nvim", -- optional
-      "ibhagwan/fzf-lua", -- optional
-    },
-    keys = {
-      { "<leader>gg", ":Neogit<CR>", desc = "Neogit toggle" },
-    },
+    -- Only one of these is needed, not both.
+    "nvim-telescope/telescope.nvim",   -- optional
+    "ibhagwan/fzf-lua",                -- optional
+  },
+  keys = {
+    { "<leader>gg", ":Neogit<CR>", desc = "Neogit toggle" },
   },
 }
