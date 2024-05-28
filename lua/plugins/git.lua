f4e3dbe496d1af65b1db1e64b710d1ee4f2f1ede
Marco Antônio <ma.souza.junior@gmail.com>
2024-05-27 21:59:47 -0300

    fix: set v0.0.1 for neogit to keep nvim 0.9

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -2,6 +2,7 @@ return {
   {
     "NeogitOrg/neogit",
     config = true,
+    tag = "v0.0.1",
     dependencies = {
       "nvim-lua/plenary.nvim", -- required
       "sindrets/diffview.nvim", -- optional - Diff integration
