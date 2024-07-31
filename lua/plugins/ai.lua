e7ec510d4c1f13fb3ec51b24821d7b34759bff4a
Marco Antônio <ma.souza.junior@gmail.com>
2024-07-31 16:54:18 -0300

    enh: update ai configs

--- lua/plugins/ai.lua
+++ lua/plugins/ai.lua
@@ -4,14 +4,14 @@ return {
   {
     "David-Kunz/gen.nvim",
     cmd = "Gen",
-    opts = { model = "llama3" },
+    opts = { model = "llama3.1" },
   },
 
   -- setup ollero
   {
     "marco-souza/ollero.nvim",
     event = "VeryLazy",
-    opts = { model = "llama3" },
+    opts = { model = "llama3.1" },
     -- dir = "~/w/marco-souza/ollero.nvim/",
     dependencies = {
       "marco-souza/term.nvim",
@@ -19,31 +19,31 @@ return {
     },
   },
 
-  -- {
-  --   "huggingface/llm.nvim",
-  --   enable = false, -- this is not working yet :(
-  --   build = ":MasonInstall llm-ls", -- ensure llm-ls is installed
-  --   version = "main",
-  --   opts = {
-  --     model = "gemma:latest",
-  --     backend = "ollama",
-  --     url = "http://localhost:11434/api/generate",
-  --     -- cf https://github.com/ollama/ollama/blob/main/docs/api.md#parameters
-  --     request_body = {
-  --       model = "gemma",
-  --       -- Modelfile options for the model you use
-  --       options = {
-  --         temperature = 0.2,
-  --         top_p = 0.95,
-  --       },
-  --     },
-  --     tokens_to_clear = { "<EOT>" },
-  --     fim = {
-  --       enabled = true,
-  --       prefix = "<PRE> ",
-  --       middle = " <MID>",
-  --       suffix = " <SUF>",
-  --     },
-  --   },
-  -- },
+  {
+    "huggingface/llm.nvim",
+    enable = false,                 -- this is not working yet :(
+    build = ":MasonInstall llm-ls", -- ensure llm-ls is installed
+    version = "main",
+    opts = {
+      model = "llama3.1:latest",
+      backend = "ollama",
+      url = "http://localhost:11434/api/generate",
+      -- cf https://github.com/ollama/ollama/blob/main/docs/api.md#parameters
+      request_body = {
+        model = "llama3.1",
+        -- Modelfile options for the model you use
+        options = {
+          temperature = 0.2,
+          top_p = 0.95,
+        },
+      },
+      tokens_to_clear = { "<EOT>" },
+      fim = {
+        enabled = true,
+        prefix = "<PRE> ",
+        middle = " <MID>",
+        suffix = " <SUF>",
+      },
+    },
+  },
 }
