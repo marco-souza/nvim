6606439f06fcf95a96010192321f6556ab43785e
Marco Antônio <ma.souza.junior@gmail.com>
2024-09-16 10:59:42 -0300

    ref: cleanup ai plugins

--- lua/plugins/ai.lua
+++ lua/plugins/ai.lua
@@ -1,12 +1,6 @@
 return {
   { "github/copilot.vim", event = "VeryLazy" },
 
-  {
-    "David-Kunz/gen.nvim",
-    cmd = "Gen",
-    opts = { model = "llama3.1" },
-  },
-
   -- setup ollero
   {
     "marco-souza/ollero.nvim",
@@ -19,31 +13,37 @@ return {
     },
   },
 
-  {
-    "huggingface/llm.nvim",
-    enable = false,                 -- this is not working yet :(
-    build = ":MasonInstall llm-ls", -- ensure llm-ls is installed
-    version = "main",
-    opts = {
-      model = "llama3.1:latest",
-      backend = "ollama",
-      url = "http://localhost:11434/api/generate",
-      -- cf https://github.com/ollama/ollama/blob/main/docs/api.md#parameters
-      request_body = {
-        model = "llama3.1",
-        -- Modelfile options for the model you use
-        options = {
-          temperature = 0.2,
-          top_p = 0.95,
-        },
-      },
-      tokens_to_clear = { "<EOT>" },
-      fim = {
-        enabled = true,
-        prefix = "<PRE> ",
-        middle = " <MID>",
-        suffix = " <SUF>",
-      },
-    },
-  },
+  -- {
+  --   "David-Kunz/gen.nvim",
+  --   cmd = "Gen",
+  --   opts = { model = "llama3.1" },
+  -- },
+
+  -- {
+  --   "huggingface/llm.nvim",
+  --   enable = false,                 -- this is not working yet :(
+  --   build = ":MasonInstall llm-ls", -- ensure llm-ls is installed
+  --   version = "main",
+  --   opts = {
+  --     model = "llama3.1:latest",
+  --     backend = "ollama",
+  --     url = "http://localhost:11434/api/generate",
+  --     -- cf https://github.com/ollama/ollama/blob/main/docs/api.md#parameters
+  --     request_body = {
+  --       model = "llama3.1",
+  --       -- Modelfile options for the model you use
+  --       options = {
+  --         temperature = 0.2,
+  --         top_p = 0.95,
+  --       },
+  --     },
+  --     tokens_to_clear = { "<EOT>" },
+  --     fim = {
+  --       enabled = true,
+  --       prefix = "<PRE> ",
+  --       middle = " <MID>",
+  --       suffix = " <SUF>",
+  --     },
+  --   },
+  -- },
 }
