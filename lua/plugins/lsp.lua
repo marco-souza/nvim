62f52ae8a47022a7ba49c0ac5a56f27e261702e9
Marco Antônio <ma.souza.junior@gmail.com>
2024-09-16 11:29:41 -0300

    ref: remove unused aliases

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -9,8 +9,4 @@ return {
       opts = { automatic_installation = true },
     },
   },
-  keys = {
-    { "gD", vim.lsp.buf.references, desc = "Go to References" },
-    { "gr", vim.lsp.buf.rename, desc = "Rename symbol" },
-  },
 }
