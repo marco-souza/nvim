d413c1b44b1b719a69a89cc2be6c572352d42d3b
Marco Antônio <ma.souza.junior@gmail.com>
2024-09-16 11:12:05 -0300

    ref: remove neorg

--- lua/plugins/notes.lua
+++ lua/plugins/notes.lua
@@ -14,32 +14,4 @@ return {
       },
     },
   },
-
-  {
-    "nvim-orgmode/orgmode",
-    event = "VeryLazy",
-    ft = { "org" },
-    config = function()
-      local notes_directory = "~/workspace/marco-souza/notes"
-      -- Setup orgmode
-      require("orgmode").setup({
-        org_hide_leading_stars = true,
-        org_agenda_files = notes_directory .. "/**/*",
-        org_default_notes_file = notes_directory .. "/notes.org",
-        org_todo_keywords = { "TODO", "WIP", "|", "DONE", "CANCELED" },
-        org_todo_keyword_faces = {
-          WIP = ":foreground yellow :weight bold",
-          CANCELED = ":foreground red :weight bold :underline on",
-          -- TODO = ":background #000000 :foreground red", -- overrides builtin color for `TODO` keyword
-        },
-      })
-
-      -- NOTE: If you are using nvim-treesitter with `ensure_installed = "all"` option
-      -- add `org` to ignore_install
-      -- require("nvim-treesitter.configs").setup({
-      --   ensure_installed = "all",
-      --   ignore_install = { "org" },
-      -- })
-    end,
-  },
 }
