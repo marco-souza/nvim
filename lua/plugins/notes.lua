0f378d8efef5f3f6bcb649e889873ee658c0cf5d
Marco Antônio <ma.souza.junior@gmail.com>
2024-10-23 10:26:57 -0300

    fix: initialize conceallevel after obsidian start

--- lua/plugins/notes.lua
+++ lua/plugins/notes.lua
@@ -2,20 +2,25 @@ return {
   {
     "epwalsh/obsidian.nvim",
     version = "*", -- recommended, use latest release instead of latest commit
-    lazy = true,
+    config = function()
+      require("obsidian").setup({
+        workspaces = {
+          { name = "notes", path = "~/w/marco-souza/notes" },
+        },
+        follow_url_func = function(url)
+          -- Open the URL in the default web browser.
+          vim.fn.jobstart({ "open", url }) -- Mac OS
+        end,
+      })
+
+      -- obsidian
+      vim.opt_local.conceallevel = 2
+    end,
+    -- lazy = true,
     ft = "markdown",
     dependencies = {
       -- Required.
       "nvim-lua/plenary.nvim",
-    },
-    opts = {
-      workspaces = {
-        { name = "notes", path = "~/w/marco-souza/notes" },
-      },
-      follow_url_func = function(url)
-        -- Open the URL in the default web browser.
-        vim.fn.jobstart({ "open", url }) -- Mac OS
-      end,
     },
   },
 }
