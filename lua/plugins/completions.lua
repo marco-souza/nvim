addcb02d5acdd6f8ba9a129d8948160044e1ffc2
Marco Antônio <ma.souza.junior@gmail.com>
2025-12-02 18:32:04 -0300

    ref: remove avante

--- lua/plugins/completions.lua
+++ lua/plugins/completions.lua
@@ -61,12 +61,6 @@ return {
           "lazydev",
           "path",
 
-          -- avante
-          "avante_commands",
-          "avante_mentions",
-          "avante_shortcuts",
-          "avante_files",
-
           -- snippets
           "copilot",
           "snippets",
@@ -85,31 +79,6 @@ return {
           lazydev = {
             name = "lazydev",
             module = "lazydev.integrations.blink",
-          },
-
-          avante_commands = {
-            name = "avante_commands",
-            module = "blink.compat.source",
-            score_offset = 90, -- show at a higher priority than lsp
-            opts = {},
-          },
-          avante_files = {
-            name = "avante_files",
-            module = "blink.compat.source",
-            score_offset = 100, -- show at a higher priority than lsp
-            opts = {},
-          },
-          avante_mentions = {
-            name = "avante_mentions",
-            module = "blink.compat.source",
-            score_offset = 1000, -- show at a higher priority than lsp
-            opts = {},
-          },
-          avante_shortcuts = {
-            name = "avante_shortcuts",
-            module = "blink.compat.source",
-            score_offset = 1000, -- show at a higher priority than lsp
-            opts = {},
           },
         },
       },
